0809dd43da957fe3b603e2345eaaa1403d9c0c7b
adonay martinez <mm3806012024@unab.edu.sv>
2026-06-07 21:10:11 -0600

    creamos el sistema de login funcional, incluyendo cuando las credenciales dan error

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def home(request):
+
+    if not request.user.is_authenticated:
+        return redirect('/login')
+
     return render(request, 'base.html')
+
+
+def login_view(request):
+
+    error = ""
+
+    if request.method == "POST":
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+        else:
+            error = "Usuario o contraseña incorrectos"
+
+    return render(request, 'login.html', {
+        'error': error
+    })
